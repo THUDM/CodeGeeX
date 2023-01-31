2d485f0610a1409f561a607e807ceb862b6d241e
Xiaoyu Zhang <35585791+BBuf@users.noreply.github.com>
2023-01-31 13:51:46 +0800

    Update test_inference.py

--- tests/test_inference.py
+++ tests/test_inference.py
@@ -137,6 +137,7 @@ def main():
     if args.quantize:
         model = quantize(model, weight_bit_width=8, backend="torch")
     model.cuda()
+    torch.cuda.synchronize()
     
     with open(args.prompt_file, "r") as f:
         prompt = f.readlines()
